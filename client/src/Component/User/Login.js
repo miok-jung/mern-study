aaa788b2630300733d3d7147a641c1f71c6a6d11
miok-jung <miok.dev@gmail.com>
2022-05-04 10:35:14 +0900

    feat: Add to Login

--- client/src/Component/User/Login.js
+++ client/src/Component/User/Login.js
@@ -1,13 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoginDiv from "../../Style/UserCSS";
+
+import firebase from "../../firebase";
 
 const Login = () => {
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
+  const [ErrorMsg, setErrorMsg] = useState("");
 
   let navigate = useNavigate();
 
+  const SignInFunc = async (e) => {
+    e.preventDefault();
+    if (!(Email && Password)) {
+      return alert("모든 값을 채워주세요.");
+    }
+    try {
+      await firebase.auth().signInWithEmailAndPassword(Email, Password);
+      navigate("/");
+    } catch (err) {
+      if (err.code === "auth/user-not-found") {
+        setErrorMsg("존재하지 않는 이메일입니다.");
+      } else if (err.code === "auth/wrong-password") {
+        setErrorMsg("비밀번호가 일치하지 않습니다.");
+      } else {
+        setErrorMsg("로그인에 실패하였습니다.");
+      }
+
+      return;
+    }
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorMsg("");
+    }, 5000);
+  }, [ErrorMsg]);
   return (
     <LoginDiv>
       <form>
@@ -29,7 +57,8 @@ const Login = () => {
             setPassword(e.currentTarget.value);
           }}
         />
-        <button>로그인</button>
+        {ErrorMsg != "" && <p>{ErrorMsg}</p>}
+        <button onClick={(e) => SignInFunc(e)}>로그인</button>
         <button
           onClick={(e) => {
             e.preventDefault();
