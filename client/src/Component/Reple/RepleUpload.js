fe2f3336ddd26d13c52daabc8b3c044832532145
miok-jung <miok.dev@gmail.com>
2022-06-11 17:42:44 +0900

    modify: Reple - 덧글 기능 일부 수정하기

--- client/src/Component/Reple/RepleUpload.js
+++ client/src/Component/Reple/RepleUpload.js
@@ -1,20 +1,30 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import userSlice from "../../Reducer/userSlice";
 
-const RepleUpload = () => {
+const RepleUpload = (props) => {
   const [Reple, setReple] = useState("");
   const user = useSelector((state) => state.user);
 
   const SubmitHandler = (e) => {
     e.preventDefault();
 
+    if (!Reple) {
+      return alert("댓글 내용을 채워주세요!");
+    }
     let body = {
       reple: Reple,
       uid: user.uid,
+      postId: props.postId,
     };
-    axios.post("/api/reple/submit", body);
+    axios.post("/api/reple/submit", body).then((res) => {
+      if (res.data.success) {
+        setReple("");
+        alert("댓글 작성이 성공하였습니다.");
+      } else {
+        alert("댓글 작성이 실패하였습니다.");
+      }
+    });
   };
   return (
     <div>
